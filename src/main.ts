464e3d381fa74ede3c3501c8fd32a7984195364c
Keven Chen <han7586917@outlook.com>
2022-06-28 11:09:35 +0800

    Setup Nestjs app

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,16 @@
-export default null;
+import { NestFactory } from '@nestjs/core';
+
+import { AppModule } from './app.module';
+
+(async function main() {
+  const { PORT, HOST } = process.env;
+  const port = Number(PORT) || 8000;
+  const host = HOST || '0.0.0.0';
+  const app = await NestFactory.create(AppModule);
+
+  // start listening
+  await app.listen(port, host);
+
+  // log server url
+  console.info(`Application is running on: ${await app.getUrl()}`);
+}());
